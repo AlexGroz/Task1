5d5f51f33d9bfb563cb95917edc5da0a56433524
AlexGroz <groznovaa@gmail.com>
2021-10-21 13:12:19 +0500

    Initial commit

---
+++ src/Module1Part1Task1.java
@@ -0,0 +1,11 @@
+public class Module1Part1Task1 {
+    public static void main(String[] args) {
+        int a = 1;
+        int b = 2;
+        int c = 3;
+        int d;
+
+        d = ((a - 3) * b /2) +  c;
+        System.out.println(d);
+    }
+}
